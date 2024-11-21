e83db4b56792c76227909d60dc3729757fbb40b4
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-11-21 15:21:00 +0530

    feat: Update product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 const upload = require("../middleware/uploadMiddleware");
+
 router.get("/", productController.getAllProducts);
 router.get("/filtered", productController.getFilteredProducts);
-router.post("/", upload.single("image"), productController.addProduct);
+
+router.post("/", upload.array("images", 5), productController.addProduct);
 router.delete("/:id", productController.deleteProduct);
-router.put("/:id", upload.single("image"), productController.updateProduct);
+router.put("/:id", upload.array("images", 5), productController.updateProduct);
 
 module.exports = router;
