ed8a3fe88c88c1a4e0f0f3715b96f281f9727ee9
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-11-23 09:55:41 +0530

    feat: Added routes for discounted & featured products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 const upload = require("../middleware/uploadMiddleware");
 
+// General product routes (FETCH ALL & FILTERED PRODUCTS)
 router.get("/", productController.getAllProducts);
 router.get("/filtered", productController.getFilteredProducts);
 
+// Specific product type routes (FETCH FEATURED & DISCOUNTED PRODUTS)
+router.get("/featured", productController.getFeaturedProducts);
+router.get("/limited-offers", productController.getLimitedOfferProducts);
+router.get("/discounted", productController.getDiscountedProducts);
+
+// CRUD operations (ADD, UPDATE, DELETE PRODUCTS)
 router.post("/", upload.array("images", 5), productController.addProduct);
-router.delete("/:id", productController.deleteProduct);
 router.put("/:id", upload.array("images", 5), productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
